cd9f8d998ff5140d2abb49b0a3e37b135a7900c5
DanielAD10 <adamsdaniel171@gmail.com>
2023-05-31 01:24:01 +0630

    Add javascript onclick function for menu list

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-document.querySelector('h1').style.display = 'block';
+const menu = document.querySelector(".menu");
+
+const openMenu = () => {
+    menu.style.display = 'block';
+}
+
+const closeMenu = () => {
+    menu.style.display = 'none';
+}
+
+document.querySelector('.nav-btn').addEventListener('click', openMenu);
+document.querySelector('.close-btn').addEventListener('click', closeMenu);
