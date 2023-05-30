1b6ebfcebfd460684e4d5c217e626676fadea9b9
DanielAD10 <adamsdaniel171@gmail.com>
2023-05-31 02:59:25 +0630

    Fix linter errors

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const menu = document.querySelector(".overlay");
+const menu = document.querySelector('.overlay');
 
 const openMenu = () => {
-    document.body.classList.add('stop-scrolling');
-    menu.style.display = 'block';
-}
+  document.body.classList.add('stop-scrolling');
+  menu.style.display = 'block';
+};
 
 const closeMenu = () => {
-    menu.style.display = 'none';
-    document.body.classList.remove('stop-scrolling');
-}
+  menu.style.display = 'none';
+  document.body.classList.remove('stop-scrolling');
+};
 
 document.querySelector('.nav-btn').addEventListener('click', openMenu);
 document.querySelector('.close-btn').addEventListener('click', closeMenu);
